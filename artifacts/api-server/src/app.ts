7b2a11da633a1ab5e356772babe243fc4d712868
ludodzsupport-maker <ludo.dz.support@gmail.com>
2026-08-08 14:54:23 +0100

    Fix pino-http import typing

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,8 +1,27 @@
-import express, { type Express } from "express";
+import express, {
+  type Express,
+  type Request,
+  type RequestHandler,
+  type Response,
+} from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import * as pinoHttpModule from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
+
+type PinoHttpRequest = Request & { id?: unknown };
+
+type PinoHttpFactory = (options: {
+  logger: typeof logger;
+  serializers: {
+    req(req: PinoHttpRequest): unknown;
+    res(res: Response): unknown;
+  };
+}) => RequestHandler;
+
+const pinoHttp = (
+  "default" in pinoHttpModule ? pinoHttpModule.default : pinoHttpModule
+) as PinoHttpFactory;
 
 const app: Express = express();
 
@@ -10,14 +29,14 @@ app.use(
   pinoHttp({
     logger,
     serializers: {
-      req(req) {
+      req(req: PinoHttpRequest) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: Response) {
         return {
           statusCode: res.statusCode,
         };
