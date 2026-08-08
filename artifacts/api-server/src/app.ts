51c444fc3dee71904204470351c9a0906bdea768
ludodzsupport-maker <ludo.dz.support@gmail.com>
2026-08-08 14:58:11 +0100

    Bypass pino-http callable type check

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,32 +1,15 @@
-import express, {
-  type Express,
-  type Request,
-  type RequestHandler,
-  type Response,
-} from "express";
+import express, { type Express, type Request, type Response } from "express";
 import cors from "cors";
-import * as pinoHttpModule from "pino-http";
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
 type PinoHttpRequest = Request & { id?: unknown };
 
-type PinoHttpFactory = (options: {
-  logger: typeof logger;
-  serializers: {
-    req(req: PinoHttpRequest): unknown;
-    res(res: Response): unknown;
-  };
-}) => RequestHandler;
-
-const pinoHttp = (
-  "default" in pinoHttpModule ? pinoHttpModule.default : pinoHttpModule
-) as PinoHttpFactory;
-
 const app: Express = express();
 
 app.use(
-  pinoHttp({
+  (pinoHttp as any)({
     logger,
     serializers: {
       req(req: PinoHttpRequest) {
